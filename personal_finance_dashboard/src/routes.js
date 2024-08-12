d9cf01224eb675388817b12161107a97073172fe
Hector Cryo <sonic200887@gmail.com>
2024-08-12 20:31:46 +0300

    Added more foundational set-up code

---
+++ personal_finance_dashboard/src/routes.js
@@ -0,0 +1,13 @@
+const ROUTES = {
+    HOME: '/',
+    REGISTER: '/register',
+    LOGIN: '/login',
+    USER: (userId) = `/${userId}`,
+    EXPENSES: '/expenses',
+    EXPENSE: (expenseId) => `/expenses/${expenseId}`,
+    INCOMES: '/income',
+    INCOME: (incomeId) => `/income/${incomeId}`,
+    INVESTMENTS: '/investments'
+};
+
+export default ROUTES;
